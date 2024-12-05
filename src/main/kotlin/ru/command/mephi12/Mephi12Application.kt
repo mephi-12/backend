e4991acbad3ab3d6730b624f2638fd4b65dd4068
Kokorev-VD <digitalfalk@mail.ru>
2024-12-05 21:15:27 +0300

    🎉 Init commit

---
+++ src/main/kotlin/ru/command/mephi12/Mephi12Application.kt
@@ -0,0 +1,11 @@
+package ru.command.mephi12
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Mephi12Application
+
+fun main(args: Array<String>) {
+    runApplication<Mephi12Application>(*args)
+}
---
+++ src/test/kotlin/ru/command/mephi12/Mephi12ApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.command.mephi12
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Mephi12ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
