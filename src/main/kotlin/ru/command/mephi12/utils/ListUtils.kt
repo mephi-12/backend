9425020b6f9a32007676bee018ec7b0c610ccaf8
Kokorev-VD <digitalfalk@mail.ru>
2024-12-09 20:46:39 +0300

    🚑️ Фикс fillUpWithZero: было некорректное поведение при листах с пустыми элементами

--- src/main/kotlin/ru/command/mephi12/utils/ListUtils.kt
+++ src/main/kotlin/ru/command/mephi12/utils/ListUtils.kt
@@ -1,6 +1,6 @@
 package ru.command.mephi12.utils
 
 fun List<String>.fillUpWithZero() : List<String> {
-    val maxLength = this.sumOf { it.toBigInteger() }.toString().length
+    val maxLength = this.sumOf { it.takeIf { it.isNotEmpty() }?.toBigInteger() ?: return this }.toString().length
     return this.map { "${"0".repeat(maxLength - it.length)}$it"}
 }
