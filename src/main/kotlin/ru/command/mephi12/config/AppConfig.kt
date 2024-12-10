c090e3d5695b76827c06db7266aab0f755cc1f07
Angoga <Anogeorgy05@gmail.com>
2024-12-10 23:18:51 +0300

    :ambulance: CORS fix

---
+++ src/main/kotlin/ru/command/mephi12/config/AppConfig.kt
@@ -0,0 +1,27 @@
+package ru.command.mephi12.config
+
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+@Configuration
+@EnableCaching
+class AppConfig : WebMvcConfigurer {
+
+    @Bean
+    fun corsFilter(): CorsFilter {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        config.addAllowedOriginPattern("*")
+        config.addAllowedHeader("*")
+        config.addAllowedMethod("*")
+        source.registerCorsConfiguration("/**", config)
+        return CorsFilter(source)
+    }
+}
