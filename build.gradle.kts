bd7744a6f1d61111c7081cb4bd695b6291ac0ea3
Kokorev-VD <digitalfalk@mail.ru>
2025-04-09 20:18:05 +0300

    🚀 Добавлен Docker

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,20 +20,27 @@ repositories {
 }
 
 dependencies {
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-
+    // Spring
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-validation")
 
+    // Utils
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("io.github.cdimascio:dotenv-java:2.2.0")
+
+    // Jwt
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
 
+    // PostgreSQL
     runtimeOnly("org.postgresql:postgresql")
+
+    // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
--- src/main/kotlin/ru/command/mephi12/Mephi12Application.kt
+++ src/main/kotlin/ru/command/mephi12/Mephi12Application.kt
@@ -1,5 +1,6 @@
 package ru.command.mephi12
 
+import io.github.cdimascio.dotenv.Dotenv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +8,16 @@ import org.springframework.boot.runApplication
 class Mephi12Application
 
 fun main(args: Array<String>) {
+//    Нужно для подгрузки переменных окружения если потребуется запустить не через контейнер
+//    loadEnvParams()
+
     runApplication<Mephi12Application>(*args)
+}
+
+private fun loadEnvParams() {
+    val dotenv = Dotenv.configure().directory("deployment").load()
+
+    dotenv.entries().forEach { envParam ->
+        System.setProperty(envParam.key, envParam.value)
+    }
 }
